b684a2342220a8083bf862ede74f2541906bb710
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-08-16 00:53:14 +0300

    known content types

--- src/known_content_types.py
+++ src/known_content_types.py
@@ -1,5 +1,16 @@
 class KnownContentTypes:
+    """
+    Known content types are usually used for Server type messages.
+    """
+
     HELLO = 101
+    """ 101 """
+
     OK = 200
+    """ 200 """
+
     ACCEPTED = 202
+    """ 202 """
+
     BAD_REQUEST = 400
+    """ 400 """
