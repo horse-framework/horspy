5f16ebc249a8d09d12770e178bf10fcff677c43b
Mehmet Helvacıköylü <mhelvacikoylu@gmail.com>
2020-08-16 11:56:00 +0300

    pull container

---
+++ src/pull_container.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from enum import Enum
+
+
+class PullProcess(Enum):
+    """ Pull request statuses"""
+
+    Receiving = 0
+    """ Still receiving messages from server """
+
+    Unacceptable = 1
+    """ Server respond unacceptable request """
+
+    Unauthorized = 2
+    """ Unauthorized process """
+
+    Empty = 3
+    """ Queue is empty """
+
+    Completed = 4
+    """ All messages are received, process completed. """
+
+    Timeout = 5
+    """ Message receive operation timed out. """
+
+    NetworkError = 6
+    """ Disconnected from server while receiving messages """
+
+
+class PullContainer:
+    """ Response container of pull request """
+
+    request_id: str
+    """ Pull request message id """
+
+    received_count: int
+    """ Received message count """
+
+    request_count: int
+    """ Requested message count """
+
+    status: PullProcess
+    """ Pull request status """
+
+    last_received: datetime
+    """ The date last message received """
